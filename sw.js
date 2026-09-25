2d2ec753c5edcaf75e518cd2fb706aa2939fe976
emptychair1 <averiearden@gmail.com>
2026-09-25 18:33:24 -0400

    Build monochrome PWA reader prototype

---
+++ sw.js
@@ -0,0 +1,1 @@
+self.addEventListener("install",e=>e.waitUntil(caches.open("house-v1").then(c=>c.addAll(["/","/index.html","/style.css","/app.js","/manifest.json","/icon.svg"]))));self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
