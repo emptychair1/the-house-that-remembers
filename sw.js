de154b2a7b33ef276ad6f29851c1be1d0d408e05
emptychair1 <averiearden@gmail.com>
2026-09-26 07:23:48 -0400

    Bump service worker marker to v229 void presence pass

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 // Service-worker kill switch for old book caches.
 // This file intentionally does not cache or serve book assets.
 
-const CURRENT_BOOK_QUERY = 'book-foreword-v228-audio-cued-phone';
+const CURRENT_BOOK_QUERY = 'book-foreword-v229-void-presence-phone';
 
 self.addEventListener('install', event => {
   self.skipWaiting();
