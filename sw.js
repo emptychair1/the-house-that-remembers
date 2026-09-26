5f3f6e59d3f4bfa1a77e6aa47a7aa054f36c886d
emptychair1 <averiearden@gmail.com>
2026-09-25 23:03:26 -0400

    Bump reader cache for Page 1 cover manifestation

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "house-rabbit-icon-v2";
+const CACHE_NAME = "house-page1-pi-manifest-v1";
 const ASSETS = [
   "./",
   "./index.html",
@@ -27,5 +27,13 @@ self.addEventListener("activate", event => {
 
 self.addEventListener("fetch", event => {
   if (event.request.method !== "GET") return;
-  event.respondWith(caches.match(event.request).then(response => response || fetch(event.request)));
+  event.respondWith(
+    fetch(event.request)
+      .then(response => {
+        const copy = response.clone();
+        caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy));
+        return response;
+      })
+      .catch(() => caches.match(event.request))
+  );
 });
