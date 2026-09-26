1b0bee95b15ef166d584c78a61857900db2372d2
emptychair1 <averiearden@gmail.com>
2026-09-25 22:18:18 -0400

    Attach rabbit PWA icon assets

--- sw.js
+++ sw.js
@@ -1,1 +1,31 @@
-self.addEventListener("install",e=>e.waitUntil(caches.open("house-v1").then(c=>c.addAll(["/","/index.html","/style.css","/app.js","/manifest.json","/icon.svg"]))));self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const CACHE_NAME = "house-rabbit-icon-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./app.js",
+  "./audio.js",
+  "./manifest.json",
+  "./static/icons/apple-touch-icon.png",
+  "./static/icons/favicon.png",
+  "./static/icons/icon-192.png",
+  "./static/icons/icon-512.png",
+  "./static/icons/maskable-512.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+  if (event.request.method !== "GET") return;
+  event.respondWith(caches.match(event.request).then(response => response || fetch(event.request)));
+});
