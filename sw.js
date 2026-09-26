12b5d3dfd2717c4484c297f88e93a3d0bd17cc62
emptychair1 <averiearden@gmail.com>
2026-09-26 06:45:13 -0400

    Bump service worker kill switch to v227

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 // Service-worker kill switch for old book caches.
 // This file intentionally does not cache or serve book assets.
 
-const CURRENT_BOOK_QUERY = 'book-foreword-v226-black-clean-audio-phone';
+const CURRENT_BOOK_QUERY = 'book-foreword-v227-void-rough-phone';
 
 self.addEventListener('install', event => {
   self.skipWaiting();
